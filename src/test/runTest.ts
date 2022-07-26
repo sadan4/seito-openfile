586cffc2c47120d941bcdd181cdfefd3c7b7440b
Fr43nk <dev.seito.de>
2022-07-26 21:16:26 +0000

    Docker modifications Update packages

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -5,7 +5,7 @@ import { runTests } from '@vscode/test-electron';
 
 async function main() {
 	try {
-		const wsRoot = process.env.WSROOT;
+		const wsRoot = process.env.WSROOT ? process.env.WSROOT : "";
 		// const wsRoot = "/builds/fr43nk/seito-openfile/Unittests-tmp";
 		console.log(`Workspace root: ${wsRoot}`);
 		
